a2d8d6e510a6304d8cf087f19dad5a5479b242c9
Hà Đạt <mrdunghr>
2023-02-23 15:16:40 +0700

    Bài Tập Sử Dụng Toán Tử

---
+++ js.js
@@ -0,0 +1,8 @@
+let inputWidth = prompt('Nhập Độ Rộng');
+document.writeln('rộng: ' + inputWidth);
+let inputHeight = prompt('Nhập Chiều Cao');
+document.writeln('Cao: ' + inputHeight);
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let arena = width * height
+document.write('diện tích hình chữ nhật ' + arena);
